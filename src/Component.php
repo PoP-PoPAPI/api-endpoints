6d7bc900d4451b146e7252077161c536d2d222db
Leonardo Losoviz <leo@getpop.org>
2020-05-27 21:28:06 +0800

    Initial commit

--- src/Component.php
+++ src/Component.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace PoP\APIEndpoints;
 
 use PoP\Root\Component\AbstractComponent;
-use PoP\APIEndpoints\EndpointHandler;
 
 /**
  * Initialize component
@@ -22,37 +21,10 @@ class Component extends AbstractComponent
     }
 
     /**
-     * All conditional component classes that this component depends upon, to initialize them
-     *
-     * @return array
-     */
-    public static function getDependedConditionalComponentClasses(): array
-    {
-        return [
-            \PoP\RESTAPI\Component::class,
-            \PoP\GraphQLAPI\Component::class,
-        ];
-    }
-
-    /**
      * Initialize services
      */
     protected static function doInitialize(array $configuration = [], bool $skipSchema = false): void
     {
         parent::doInitialize($configuration, $skipSchema);
-        ComponentConfiguration::setConfiguration($configuration);
-    }
-
-    /**
-     * Boot component
-     *
-     * @return void
-     */
-    public static function beforeBoot(): void
-    {
-        parent::beforeBoot();
-
-        // Initialize services
-        (new EndpointHandler())->init();
     }
 }
